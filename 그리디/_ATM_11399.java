1beae675f8f13c3a644127d8c3fb8e1bec7a761e
tpgjs0527 <tpgjs0527@naver.com>
2022-10-21 17:12:18 +0900

    백준_ATM_11399

---
+++ 그리디/_ATM_11399.java
@@ -0,0 +1,33 @@
+package _그리디;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _ATM_11399 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(a);
+		
+		int ans = 0;
+		for (int i = 0; i < n; i++) {
+			ans += a[i]*(n-i);
+		}
+		
+		System.out.println(ans);
+	}
+
+}
